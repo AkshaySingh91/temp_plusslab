fd0ffd816d890d5bc196f3a1e14260397309c2e2
Rohit tiwari <rt30082004@gmail.com>
2025-02-09 12:55:22 +0530

    testing collaboration

--- plusslabs-backend/config/db.js
+++ plusslabs-backend/config/db.js
@@ -12,5 +12,16 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-
-module.exports = connectDB;
+const connectDB1 = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log(`MongoDB Connected: ${conn.connection.host}`);
+    } catch (error) {
+        console.error(`Error: ${error.message}`);
+        process.exit(1);
+    }
+};
+module.exports = connectDB1;
