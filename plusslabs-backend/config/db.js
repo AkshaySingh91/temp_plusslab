ae4409a4368079f68a6de6bb5635cb64aecf84b2
Shubhamsingh2110 <singhshubham1975@gmail.com>
2025-02-09 13:02:13 +0530

    test

--- plusslabs-backend/config/db.js
+++ plusslabs-backend/config/db.js
@@ -12,16 +12,4 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-const connectDB1 = async () => {
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-        });
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-    } catch (error) {
-        console.error(`Error: ${error.message}`);
-        process.exit(1);
-    }
-};
-module.exports = connectDB1;
+module.exports = connectDB;
